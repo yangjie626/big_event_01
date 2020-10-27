be96d02cc3af0702b96b9ceb060d1d7727517f3b
unknown <you@example.com>
2020-10-27 18:23:24 +0800

    123

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,4 @@
-// $.ajaxPrefilter()要绑定在所有ajax之前,这个方法会在ajax请求执行后再触发,这个方法执行完毕,ajax才执行
+// $.ajaxPrefilter()要绑定在所有ajax之前,这个方法会在ajax请求执行后再触发,拦截ajax发送执行此函数,这个方法执行完毕,ajax才真正执行
 // 开发环境地址
 var baseURL = 'http://ajax.frontend.itheima.net'
     // 测试环境地址
@@ -6,9 +6,19 @@ var baseURL = 'http://ajax.frontend.itheima.net'
     // 生产环境地址
     // var baseURL = 'http://ajax.frontend.itheima.net'
     // 拦截所有ajax请求,get/post/ajax
-$.ajaxPrefilter(function(params) {
-    // console.log(params.url);
+$.ajaxPrefilter(function(option) {
+    // console.log(option.url);
     // 拼接对应环境的服务器地址
-    params.url = baseURL + params.url
-        // console.log(params.url);
+    // option每一次阻截的ajax请求
+    option.url = baseURL + option.url
+        // console.log(option.url);
+
+
+    // 
+    if (option.url.indexOf('/my/') !== -1) {
+        // 有my,即有权限
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+// 入口函数
+$(function() {
+        // 获取用户信息,渲染头像和用户名
+        getUserInfo()
+
+
+    })
+    // 必须定义在入口函数之外,是全局函数,后面要用,使其全局可用,为了改一个图片,另一处图片跟着改动
+    // 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers要设置是api文档说明中要求的
+        /*  headers: {
+             // 重新登录,因为token过期时间12小时
+             Authorization: localStorage.getItem('token') || ""
+         }, */
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // 调用renderAvatar渲染用户的头像   
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染用户的头像函数
+function renderAvatar(user) {
+    //1. 获取用户名称
+    var name = user.nickname || user.username;
+    // 2.欢迎文字
+    $('#welcome').html(name)
+        // 3.头像
+    if (user.user_pic !== null) {
+        // 有头像,显示头像隐藏文字圆
+        $('.text-avatar').hide();
+        $('.layui-nav-img').show().attr('src', 'user.user_pic')
+
+    } else {
+        // 没有头像
+        var text = name[0].toUpperCase()
+        $('.text-avatar').show().html(text)
+        $('.layui-nav-img').hide()
+    }
+}
